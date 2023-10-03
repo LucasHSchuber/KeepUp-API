acf6103766f58de78fd1b03841f1256758b96295
Lucas Schuber <lucas.hammarstrand@hotmail.com>
2023-10-03 12:03:33 +0200

    ER, tables - created

---
+++ stock-api/database/migrations/2023_10_03_093411_create_stocks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->string('SKU', 64);
+            $table->string('name', 64);
+            $table->string('category', 64);
+            $table->string('description', 256);
+            $table->integer('price');
+            $table->integer('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
